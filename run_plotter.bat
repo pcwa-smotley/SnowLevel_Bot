e8d54717e801e935f481098f4058813b45120271
pcwa-smotley <smotley@mac.com>
2025-10-21 11:42:44 -0700

    Final Version

--- run_plotter.bat
+++ run_plotter.bat
@@ -1,11 +1,15 @@
 set err.log=U:\Documents\Programming\PycharmProjects\Prod\SnowLevel_Bot\err.log
-Rem set python=C:\Users\smotley\AppData\Local\Continuum\anaconda3\envs\Data_Grabber\python.exe
+rem --- where Conda probably lives ---
+set "CONDA_HOME=%USERPROFILE%\miniconda3"
+if not exist "%CONDA_HOME%\condabin\conda.bat" set "CONDA_HOME=%USERPROFILE%\AppData\Local\miniconda3"
+
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 @echo Current Date And Time Is: %mydate%_%mytime% >> U:\Documents\Programming\PycharmProjects\Prod\SnowLevel_Bot\err.log 2>&1
 @echo Starting...>> U:\Documents\Programming\PycharmProjects\Prod\SnowLevel_Bot\err.log 2>&1
-call activate Data_Grabber
+rem --- activate and run ---
+call "%CONDA_HOME%\condabin\conda.bat" activate plotly_python
 python U:\Documents\Programming\PycharmProjects\Prod\SnowLevel_Bot\snow_level_plotter.py >> U:\Documents\Programming\PycharmProjects\Prod\SnowLevel_Bot\err.log 2>&1
 @echo FINISHED
 @echo *****************************************************************
